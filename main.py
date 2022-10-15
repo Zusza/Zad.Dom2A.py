5605cf7d729541db13c0faada84e80fad8e9c746
Zusza <114587290+Zusza@users.noreply.github.com>
2022-10-15 13:15:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+# W zadaniach musiałam pozmieniać zmienne.
+# Zad1
+# a = int(input("Podaj liczbe: "))
+# b = int(input("Podaj liczbe: "))
+# if ((a+b)%2 == 0):
+#   print("Tak")
+# else:
+#   print("Nie")
+# Zad2
+# c = int(input("Podaj liczbe: "))
+# g = int(input("Podaj liczbe: "))
+# if ((c*g)/2) >= ((c*g)**1/2):
+#   print("Tak")
+# else:
+#   print("Nie")
+# Zad3
+# k = int(input("Podaj liczbe: "))
+# l = int(input("Podaj liczbe: "))
+# m = int(input("Podaj liczbe: "))
+# if (k == l or l == m or k == m):
+#   print()
+# else:
+#  print("Nie")
+# if (k == l):
+#  print("Tak")
+#  print(k,l)
+# if (l == m):
+#   print("Tak")
+#   print(l,m)
+# if (k == m):
+#   print("Tak")
+#   print(k,m)
+# Zad4
+# z = int(input("Podaj liczbe: "))
+# y = int(input("Podaj liczbe: "))
+# x = int(input("Podaj liczbe: "))
+# w = int(input("Podaj liczbe: "))
+# najmniejsza = None
+# for i in z, y, x, w:
+#   if najmniejsza == None or najmniejsza > i:
+#     najmniejsza = i
+# print("Najmniejsza liczba: ", najmniejsza)
+# Zad5
+# m = int(input("Podaj liczbę: "))
+# n = int(input("Podaj liczbe: "))
+# o = int(input("Podaj liczbe: "))
+# if(m + n > o and m + o > n and n + o > m):
+#   print("Tak")
+# else:
+#   print("Nie")
+# Zad6
+# s = int(input("Podaj liczbę: "))
+# r = int(input("Podaj liczbe: "))
+# p = int(input("Podaj liczbe: "))
+# if ((s + r > p) and (s + p > r) and (r + p > s)):
+#     if ((s * s + r * r == p * p) or (s * s + p * p == r * r) or (p * p + r * r == s * s)):
+#         print("Trójkąt prostokątny")
+#     else:
+#         if ((s * s + r * r < p * p) or (s * s + p * p < r * r) or (p * p + r * r < s * s)):
+#             print("Trójkąt rozwartokątny")
+#         else:
+#             print("Trójkąt ostrokątny")
+# else:
+#     print("Nie da się zbudować trójkąta")
